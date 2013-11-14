34a71fa300da0bf07dab29886f11b8903a5e25c0
Ryan Davis <ryan@acceleration.net>
2013-11-14 17:07:32 -0500

    fix false positive from gitignore.io
    refs #713

---
+++ src/Tests/Build/VersioningTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Acceleration.Build;
+using NUnit.Framework;
+
+namespace Tests.Build {
+    public class VersioningTests : Base {
+        [Test]
+        public void VersionInfo() {
+            var info = Versioning.VersionInfo(Assembly.GetAssembly(typeof(Versioning)));
+            Assert.IsNotNull(info);
+            Assert.IsNotNull(info.InformationalVersion);
+            Assert.IsNotNull(info.Version);
+        }
+
+        [Test]
+        public void VersionInfoNoInfo() {
+            var info = Versioning.VersionInfo(Assembly.GetExecutingAssembly());
+            Assert.IsNotNull(info);
+            Assert.IsNull(info.InformationalVersion);
+            Assert.IsNotNull(info.Version);
+        }
+    }
+}
