0ac9b673b7a2e137d409526bf96c79b0f4164843
Ryan Davis <ryan@acceleration.net>
2013-11-14 16:49:25 -0500

    Tests: adding tests for everything so far
    refs #713

---
+++ src/Tests/Core/ReflectionExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Acceleration.Extensions;
+using System.Reflection;
+
+namespace Tests.Core {
+    public class ReflectionExtensionsTests : Base {
+
+        Assembly assembly;
+
+        [SetUp]
+        public void Setup() {
+            assembly = Assembly.GetAssembly(typeof(ReflectionExtensions));
+        }
+
+        class DummyAttribute : Attribute { }
+
+        [Test]
+        public void GetAttribute() {
+            Assert.IsNotNull(assembly.GetAttribute<AssemblyInformationalVersionAttribute>());
+
+            Assert.IsNull(assembly.GetAttribute<DummyAttribute>());
+
+        }
+
+    }
+}
