ae8d7d3ef273a68d978cee0ee0d251275bccc061
Ryan Davis <ryan@acceleration.net>
2013-11-14 13:23:03 -0500

    start reflection extentions
    import a generalized form of `GetAttribute<T>` to fetch a particular
    attribute
    
    refs #713 (.25)

---
+++ src/Acceleration/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Acceleration.Extensions {
+    public static class ReflectionExtensions {
+        /// <summary>
+        /// Fetch an attribute 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        public static T GetAttribute<T>(this ICustomAttributeProvider provider) where T : Attribute {
+            if (provider == null) throw new ArgumentNullException("provider");
+            var matches = provider.GetCustomAttributes(typeof(T), true);
+            return matches.Select(m => m as T).FirstOrDefault(m => m != null);
+        }
+    }
+}
