c9b37d7a9ee0f44a0457564682b2cf4c73a95f96
Ryan Davis <ryan@acceleration.net>
2013-11-18 13:53:47 -0500

    core: `ICollection` extensions
    new extensions to add many items in one call:
    
     * `ICollection<T>.Add(IEnumerable<T>)`
     * `ICollection<T>.Add(params T[])`
    
    imported from ifas-commitments
    
    refs #713

---
+++ src/Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Acceleration.Extensions {
+    /// <summary>
+    /// add-ons to <see cref="ICollection"/>
+    /// </summary>
+    public static class CollectionExtensions {
+        /// <summary>
+        /// Add to a collection in bulk
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="items"></param>
+        /// <returns>how many items were added</returns>
+        public static int Add<T>(this ICollection<T> collection, IEnumerable<T> items) {
+            if (collection == null) throw new ArgumentNullException("collection");
+
+            var count = 0;
+            foreach (var item in items) { collection.Add(item); count++; }
+            return count;
+        }
+
+        /// <summary>
+        /// Add to a collection in bulk
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="items"></param>
+        /// <returns>how many items were added</returns>
+        public static int Add<T>(this ICollection<T> collection, params T[] items) {
+            if (collection == null) throw new ArgumentNullException("collection");
+            return collection.Add(items.AsEnumerable());
+        }
+    }
+}
---
+++ src/Tests/Core/CollectionExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Acceleration.Extensions;
+
+namespace Tests.Core {
+    class CollectionExtensionsTests : Base {
+
+        
+        [Test]
+        public void Add() {
+            ICollection<int> a = new List<int>();
+            Assert.IsEmpty(a);
+            a.Add(1);
+            Assert.AreEqual(1, a.Count);
+            Assert.AreEqual(2, a.Add(2, 3));
+            Assert.AreEqual(3, a.Count);
+            Assert.AreEqual(3, a.Add(a.ToArray().AsEnumerable()));
+            Assert.AreEqual(6, a.Count);
+        }
+    }
+}
