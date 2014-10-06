8038504732e14fe3d79752ae32849af30221d395
Ryan Davis <ryan@acceleration.net>
2014-10-06 12:44:29 -0400

    package for nuget with strong names
    Uses a `SIGNED` compiler flag to conditionally compile the
    `InternalsVisibleToAttribute` needed for testing, and some other flags
    in msbuild to sign the assembly when compiling it for nuget.
    
    This probably needs more work, I predict trouble when trying to compile
    a library with local dependencies like `Acceleration.Hmac`, which wants
    `Acceleration.Collections`. I guess if everything gets installed via
    nuget that might work, albeit a little silly.
    
    Also splits the `Acceleration.MappedReader` tests into their own
    assembly. Initially done attempting to get the strong names happy, but
    ended up not doing that.

--- src/Acceleration.MappedReader.Tests/Properties/AssemblyInfo.cs
+++ src/Acceleration.MappedReader.Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Acceleration.MappedReader")]
+[assembly: AssemblyTitle("Acceleration.MappedReader.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Acceleration.net")]
-[assembly: AssemblyProduct("Acceleration.MappedReader")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acceleration.MappedReader.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2f39be2f-849e-453b-a199-b39b4801fc2a")]
+[assembly: Guid("e45d5a77-770e-4848-9d20-6221dd9f9f03")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,11 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
-[assembly: CLSCompliant(true)]
-
-#if SIGNED
-#else 
-[assembly: InternalsVisibleTo("Acceleration.MappedReader.Tests")]
-#endif
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
